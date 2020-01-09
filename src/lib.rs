dc41b52e662719bb3660f353af8ba45c86d8ff31
Wesley Moore <wes@wezm.net>
2020-01-09 15:45:38 +1100

    Update to Unicode 12.1 with yeslogic-ucd-generate

--- src/lib.rs
+++ src/lib.rs
@@ -8,35 +8,62 @@
 // except according to those terms.
 
 pub mod tables;
-pub use tables::{Script, get_script, UNICODE_VERSION};
+pub use tables::Script;
+
+/// The version of [Unicode](http://www.unicode.org/)
+/// that this version of unicode-script is based on.
+pub const UNICODE_VERSION: (u64, u64, u64) = (12, 1, 0);
 
 #[cfg(feature = "harfbuzz")]
 extern crate harfbuzz_sys;
 
+fn bsearch_range_value_table(c: char, r: &'static [(u32, u32, Script)]) -> Script {
+    use ::std::cmp::Ordering::{Equal, Less, Greater};
+    let c = c as u32;
+    match r.binary_search_by(|&(lo, hi, _)| {
+        if lo <= c && c <= hi { Equal }
+        else if hi < c { Less }
+        else { Greater }
+    }) {
+        Ok(idx) => {
+            let (_, _, cat) = r[idx];
+            cat
+        }
+        Err(_) => Script::Unknown
+    }
+}
+
+/// Find the script of a single char.
+pub fn get_script(c: char) -> Script {
+    bsearch_range_value_table(c, tables::SCRIPT)
+}
+
 impl Script {
     #[cfg(feature = "harfbuzz")]
     pub fn to_hb_script(self) -> harfbuzz_sys::hb_script_t {
-        use Script::*;
         use harfbuzz_sys::*;
+        use Script::*;
         match self {
+            Adlam => HB_SCRIPT_ADLAM,
             Ahom => HB_SCRIPT_AHOM,
-            Anatolian_Hieroglyphs => HB_SCRIPT_ANATOLIAN_HIEROGLYPHS,
+            AnatolianHieroglyphs => HB_SCRIPT_ANATOLIAN_HIEROGLYPHS,
             Arabic => HB_SCRIPT_ARABIC,
             Armenian => HB_SCRIPT_ARMENIAN,
             Avestan => HB_SCRIPT_AVESTAN,
             Balinese => HB_SCRIPT_BALINESE,
             Bamum => HB_SCRIPT_BAMUM,
-            Bassa_Vah => HB_SCRIPT_BASSA_VAH,
+            BassaVah => HB_SCRIPT_BASSA_VAH,
             Batak => HB_SCRIPT_BATAK,
             Bengali => HB_SCRIPT_BENGALI,
+            Bhaiksuki => HB_SCRIPT_BHAIKSUKI,
             Bopomofo => HB_SCRIPT_BOPOMOFO,
             Brahmi => HB_SCRIPT_BRAHMI,
             Braille => HB_SCRIPT_BRAILLE,
             Buginese => HB_SCRIPT_BUGINESE,
             Buhid => HB_SCRIPT_BUHID,
-            Canadian_Aboriginal => HB_SCRIPT_CANADIAN_SYLLABICS,
+            CanadianAboriginal => HB_SCRIPT_CANADIAN_SYLLABICS,
             Carian => HB_SCRIPT_CARIAN,
-            Caucasian_Albanian => HB_SCRIPT_CAUCASIAN_ALBANIAN,
+            CaucasianAlbanian => HB_SCRIPT_CAUCASIAN_ALBANIAN,
             Chakma => HB_SCRIPT_CHAKMA,
             Cham => HB_SCRIPT_CHAM,
             Cherokee => HB_SCRIPT_CHEROKEE,
@@ -47,9 +74,11 @@ impl Script {
             Cyrillic => HB_SCRIPT_CYRILLIC,
             Deseret => HB_SCRIPT_DESERET,
             Devanagari => HB_SCRIPT_DEVANAGARI,
+            Dogra => HB_SCRIPT_DOGRA,
             Duployan => HB_SCRIPT_DUPLOYAN,
-            Egyptian_Hieroglyphs => HB_SCRIPT_EGYPTIAN_HIEROGLYPHS,
+            EgyptianHieroglyphs => HB_SCRIPT_EGYPTIAN_HIEROGLYPHS,
             Elbasan => HB_SCRIPT_ELBASAN,
+            Elymaic => HB_SCRIPT_ELYMAIC,
             Ethiopic => HB_SCRIPT_ETHIOPIC,
             Georgian => HB_SCRIPT_GEORGIAN,
             Glagolitic => HB_SCRIPT_GLAGOLITIC,
@@ -57,22 +86,24 @@ impl Script {
             Grantha => HB_SCRIPT_GRANTHA,
             Greek => HB_SCRIPT_GREEK,
             Gujarati => HB_SCRIPT_GUJARATI,
+            GunjalaGondi => HB_SCRIPT_GUNJALA_GONDI,
             Gurmukhi => HB_SCRIPT_GURMUKHI,
             Han => HB_SCRIPT_HAN,
             Hangul => HB_SCRIPT_HANGUL,
+            HanifiRohingya => HB_SCRIPT_HANIFI_ROHINGYA,
             Hanunoo => HB_SCRIPT_HANUNOO,
             Hatran => HB_SCRIPT_HATRAN,
             Hebrew => HB_SCRIPT_HEBREW,
             Hiragana => HB_SCRIPT_HIRAGANA,
-            Imperial_Aramaic => HB_SCRIPT_IMPERIAL_ARAMAIC,
+            ImperialAramaic => HB_SCRIPT_IMPERIAL_ARAMAIC,
             Inherited => HB_SCRIPT_INHERITED,
-            Inscriptional_Pahlavi => HB_SCRIPT_INSCRIPTIONAL_PAHLAVI,
-            Inscriptional_Parthian => HB_SCRIPT_INSCRIPTIONAL_PARTHIAN,
+            InscriptionalPahlavi => HB_SCRIPT_INSCRIPTIONAL_PAHLAVI,
+            InscriptionalParthian => HB_SCRIPT_INSCRIPTIONAL_PARTHIAN,
             Javanese => HB_SCRIPT_JAVANESE,
             Kaithi => HB_SCRIPT_KAITHI,
             Kannada => HB_SCRIPT_KANNADA,
             Katakana => HB_SCRIPT_KATAKANA,
-            Kayah_Li => HB_SCRIPT_KAYAH_LI,
+            KayahLi => HB_SCRIPT_KAYAH_LI,
             Kharoshthi => HB_SCRIPT_KHAROSHTHI,
             Khmer => HB_SCRIPT_KHMER,
             Khojki => HB_SCRIPT_KHOJKI,
@@ -81,19 +112,23 @@ impl Script {
             Latin => HB_SCRIPT_LATIN,
             Lepcha => HB_SCRIPT_LEPCHA,
             Limbu => HB_SCRIPT_LIMBU,
-            Linear_A => HB_SCRIPT_LINEAR_A,
-            Linear_B => HB_SCRIPT_LINEAR_B,
+            LinearA => HB_SCRIPT_LINEAR_A,
+            LinearB => HB_SCRIPT_LINEAR_B,
             Lisu => HB_SCRIPT_LISU,
             Lycian => HB_SCRIPT_LYCIAN,
             Lydian => HB_SCRIPT_LYDIAN,
             Mahajani => HB_SCRIPT_MAHAJANI,
+            Makasar => HB_SCRIPT_MAKASAR,
             Malayalam => HB_SCRIPT_MALAYALAM,
             Mandaic => HB_SCRIPT_MANDAIC,
             Manichaean => HB_SCRIPT_MANICHAEAN,
-            Meetei_Mayek => HB_SCRIPT_MEETEI_MAYEK,
-            Mende_Kikakui => HB_SCRIPT_MENDE_KIKAKUI,
-            Meroitic_Cursive => HB_SCRIPT_MEROITIC_CURSIVE,
-            Meroitic_Hieroglyphs => HB_SCRIPT_MEROITIC_HIEROGLYPHS,
+            Marchen => HB_SCRIPT_MARCHEN,
+            MasaramGondi => HB_SCRIPT_MASARAM_GONDI,
+            Medefaidrin => HB_SCRIPT_MEDEFAIDRIN,
+            MeeteiMayek => HB_SCRIPT_MEETEI_MAYEK,
+            MendeKikakui => HB_SCRIPT_MENDE_KIKAKUI,
+            MeroiticCursive => HB_SCRIPT_MEROITIC_CURSIVE,
+            MeroiticHieroglyphs => HB_SCRIPT_MEROITIC_HIEROGLYPHS,
             Miao => HB_SCRIPT_MIAO,
             Modi => HB_SCRIPT_MODI,
             Mongolian => HB_SCRIPT_MONGOLIAN,
@@ -101,25 +136,31 @@ impl Script {
             Multani => HB_SCRIPT_MULTANI,
             Myanmar => HB_SCRIPT_MYANMAR,
             Nabataean => HB_SCRIPT_NABATAEAN,
-            New_Tai_Lue => HB_SCRIPT_NEW_TAI_LUE,
+            Nandinagari => HB_SCRIPT_NANDINAGARI,
+            NewTaiLue => HB_SCRIPT_NEW_TAI_LUE,
+            Newa => HB_SCRIPT_NEWA,
             Nko => HB_SCRIPT_NKO,
+            Nushu => HB_SCRIPT_NUSHU,
+            NyiakengPuachueHmong => HB_SCRIPT_NYIAKENG_PUACHUE_HMONG,
             Ogham => HB_SCRIPT_OGHAM,
-            Ol_Chiki => HB_SCRIPT_OL_CHIKI,
-            Old_Hungarian => HB_SCRIPT_OLD_HUNGARIAN,
-            Old_Italic => HB_SCRIPT_OLD_ITALIC,
-            Old_North_Arabian => HB_SCRIPT_OLD_NORTH_ARABIAN,
-            Old_Permic => HB_SCRIPT_OLD_PERMIC,
-            Old_Persian => HB_SCRIPT_OLD_PERSIAN,
-            Old_South_Arabian => HB_SCRIPT_OLD_SOUTH_ARABIAN,
-            Old_Turkic => HB_SCRIPT_OLD_TURKIC,
+            OlChiki => HB_SCRIPT_OL_CHIKI,
+            OldHungarian => HB_SCRIPT_OLD_HUNGARIAN,
+            OldItalic => HB_SCRIPT_OLD_ITALIC,
+            OldNorthArabian => HB_SCRIPT_OLD_NORTH_ARABIAN,
+            OldPermic => HB_SCRIPT_OLD_PERMIC,
+            OldPersian => HB_SCRIPT_OLD_PERSIAN,
+            OldSogdian => HB_SCRIPT_OLD_SOGDIAN,
+            OldSouthArabian => HB_SCRIPT_OLD_SOUTH_ARABIAN,
+            OldTurkic => HB_SCRIPT_OLD_TURKIC,
             Oriya => HB_SCRIPT_ORIYA,
+            Osage => HB_SCRIPT_OSAGE,
             Osmanya => HB_SCRIPT_OSMANYA,
-            Pahawh_Hmong => HB_SCRIPT_PAHAWH_HMONG,
+            PahawhHmong => HB_SCRIPT_PAHAWH_HMONG,
             Palmyrene => HB_SCRIPT_PALMYRENE,
-            Pau_Cin_Hau => HB_SCRIPT_PAU_CIN_HAU,
-            Phags_Pa => HB_SCRIPT_PHAGS_PA,
+            PauCinHau => HB_SCRIPT_PAU_CIN_HAU,
+            PhagsPa => HB_SCRIPT_PHAGS_PA,
             Phoenician => HB_SCRIPT_PHOENICIAN,
-            Psalter_Pahlavi => HB_SCRIPT_PSALTER_PAHLAVI,
+            PsalterPahlavi => HB_SCRIPT_PSALTER_PAHLAVI,
             Rejang => HB_SCRIPT_REJANG,
             Runic => HB_SCRIPT_RUNIC,
             Samaritan => HB_SCRIPT_SAMARITAN,
@@ -127,19 +168,22 @@ impl Script {
             Sharada => HB_SCRIPT_SHARADA,
             Shavian => HB_SCRIPT_SHAVIAN,
             Siddham => HB_SCRIPT_SIDDHAM,
-            SignWriting => HB_SCRIPT_SIGNWRITING,
+            Signwriting => HB_SCRIPT_SIGNWRITING,
             Sinhala => HB_SCRIPT_SINHALA,
-            Sora_Sompeng => HB_SCRIPT_SORA_SOMPENG,
+            Sogdian => HB_SCRIPT_SOGDIAN,
+            SoraSompeng => HB_SCRIPT_SORA_SOMPENG,
+            Soyombo => HB_SCRIPT_SOYOMBO,
             Sundanese => HB_SCRIPT_SUNDANESE,
-            Syloti_Nagri => HB_SCRIPT_SYLOTI_NAGRI,
+            SylotiNagri => HB_SCRIPT_SYLOTI_NAGRI,
             Syriac => HB_SCRIPT_SYRIAC,
             Tagalog => HB_SCRIPT_TAGALOG,
             Tagbanwa => HB_SCRIPT_TAGBANWA,
-            Tai_Le => HB_SCRIPT_TAI_LE,
-            Tai_Tham => HB_SCRIPT_TAI_THAM,
-            Tai_Viet => HB_SCRIPT_TAI_VIET,
+            TaiLe => HB_SCRIPT_TAI_LE,
+            TaiTham => HB_SCRIPT_TAI_THAM,
+            TaiViet => HB_SCRIPT_TAI_VIET,
             Takri => HB_SCRIPT_TAKRI,
             Tamil => HB_SCRIPT_TAMIL,
+            Tangut => HB_SCRIPT_TANGUT,
             Telugu => HB_SCRIPT_TELUGU,
             Thaana => HB_SCRIPT_THAANA,
             Thai => HB_SCRIPT_THAI,
@@ -149,8 +193,10 @@ impl Script {
             Ugaritic => HB_SCRIPT_UGARITIC,
             Unknown => HB_SCRIPT_UNKNOWN,
             Vai => HB_SCRIPT_VAI,
-            Warang_Citi => HB_SCRIPT_WARANG_CITI,
+            Wancho => HB_SCRIPT_WANCHO,
+            WarangCiti => HB_SCRIPT_WARANG_CITI,
             Yi => HB_SCRIPT_YI,
+            ZanabazarSquare => HB_SCRIPT_ZANABAZAR_SQUARE,
         }
     }
 }
@@ -164,5 +210,6 @@ mod test {
         assert_eq!(get_script('a'), Script::Latin);
         assert_eq!(get_script('.'), Script::Common);
         assert_eq!(get_script('カ'), Script::Katakana);
+        assert_eq!(get_script('🦳'), Script::Common);
     }
 }
